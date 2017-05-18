90274dbe7afb7499d8f41aac6d374a0b3026d7a6
czheo <czheo1987@gmail.com>
2017-05-18 10:02:15 +0000

    prototype

--- clslua.lua
+++ clslua.lua
@@ -1,34 +1,14 @@
-local mp = require 'MessagePack'
-
-local function argerror(caller, narg, extramsg)
-  error("bad argument #" .. tostring(narg) .. " to "
-    .. caller .. " (" .. extramsg .. ")")
-end
-
-local function typeerror (caller, narg, arg, tname)
-  argerror(caller, narg, tname .. " expected, got " .. type(arg))
-end
-
-local function checktype (caller, narg, arg, tname)
-  if type(arg) ~= tname then
-    typeerror(caller, narg, arg, tname)
-  end
-end
+local json = require "json"
 
 local exec
 exec = function(ioctx, oid, script, func, input)
-  checktype('exec', 1, ioctx, 'userdata')
-  checktype('exec', 2, oid, 'string')
-  checktype('exec', 3, script, 'string')
-  checktype('exec', 4, func, 'string')
-  checktype('exec', 5, input, 'string')
-
-  -- build the cls_lua command
-  cmd = {[1] = script, [2] = func, [3] = input}
-  packed_input = mp.pack(cmd)
-
-  -- run script on osd
-  ret, outdata = ioctx:exec(oid, "lua", "eval", packed_input, #packed_input)
+  cmd = {
+    script = script,
+    handler = func, 
+    input = input,
+  }
+  packed_input = json.encode(cmd)
+  ret, outdata = ioctx:exec(oid, "lua", "eval_json", packed_input, #packed_input)
   return ret, outdata
 end
 
--- main.lua
+++ main.lua
@@ -1,41 +1,152 @@
+#! /usr/bin/env lua
+
 rados = require 'rados'
 clslua = require 'clslua'
+sha2 = require 'sha2'
 
+-- common args
 command = arg[1]
 pool = arg[2]
 object = arg[3]
+vpool = pool .. '.ver'
 
+-- connect to cluster
 cluster = rados.create()
 cluster:conf_read_file()
 cluster:connect()
-ioctx = cluster:open_ioctx(pool)
 
-function write(object, str, offset)
-	print('write', object, str, offset)	
+-- open pools
+-- data pool
+ioctx = cluster:open_ioctx(pool)
+if not ioctx then
+  print(pool .. ' does not exist! Do:')
+  print('rados mkpool ' .. pool)
+  return
 end
 
-function write_full(object, str)
-	print('write_full', object, str)	
+-- data.ver pool
+vioctx = cluster:open_ioctx(vpool)
+if not vioctx then
+  print(vpool .. ' does not exist! Do:')
+  print('rados mkpool ' .. vpool)
+  return
+end
+
+
+function write(object, str, offset)
+  ioctx:write(object, str, #str, offset)
+end
+
+function commit(object)
+  size, mtime = ioctx:stat(object)
+  if not size then
+    print(object .. ' does not exist')
+    return
+  end
+
+  head = ioctx:getxattr(object, 'HEAD')
+  data = ioctx:read(object, size, 0)
+  hash = sha2.sha256hex(data)
+
+  if hash == head then
+    -- file not changed since previous commit
+    print('nothing to commit')
+  else
+    -- write data to data.ver pool
+    vobj = object .. '.' .. hash
+    size, mtime = vioctx:stat(vobj)
+    if not size then
+      vioctx:write(vobj, data, #data, 0)
+      if head then
+        vioctx:setxattr(vobj, 'PREV', head, #head)
+      end
+    end
+
+    -- update prev pointer
+    ioctx:setxattr(object, 'HEAD', hash, #hash)
+  end
+
+  return hash
 end
 
 function read(object)
-	print('read', object)	
+  size, mtime = ioctx:stat(object)
+  return ioctx:read(object, size, 0)
+end
+
+function read_version(object, version)
+  vobj = object .. '.' .. version
+  size, mtime = vioctx:stat(vobj)
+  print(vioctx:read(vobj, size, 0))
 end
 
 function ls_versions(object)
-	print('ls_versions', object)	
+  size, mtime = ioctx:stat(object)
+  if not size then
+    print(object .. ' does not exist')
+    return
+  end
+
+  head = ioctx:getxattr(object, 'HEAD')
+  print(head)
+
+  vobj = object .. '.' .. head
+  prev = vioctx:getxattr(vobj, 'PREV')
+  while prev do
+    print(prev)
+    vobj = object .. '.' .. prev 
+    prev = vioctx:getxattr(vobj, 'PREV')
+  end
 end
 
 function remove(object)
-	print('ls_versions', object)	
+  size, mtime = ioctx:stat(object)
+  if not size then
+    print(object .. ' does not exist')
+    return
+  end
+
+  -- set delete flag
+  ioctx:setxattr(object, 'DELETE', 1, 1)
+end
+
+function put(object, path)
+  file = io.open(path, "r")
+  if not file then
+    print(path .. ' not found')
+    return
+  end
+  data = file:read "*a" -- *a or *all reads the whole file
+  file:close()
+  write(object, data, 0)
+  print(commit(object))
+end
+
+function get(object)
+  size, mtime = ioctx:stat(object)
+  if not size then
+    print(object .. ' does not exist')
+    return
+  end
+  del_flag = ioctx:getxattr(object, 'DELETE')
+  if del_flag then
+    print(object .. ' is deleted')
+    return
+  end
+  print(read(object))
+end
+
+function rollback(object, ver)
+  print('rollback to a specific version')
 end
 
 actions = {
-	["write"] = function() write(object, arg[4], tonumber(arg[5])) end,
-	["write_full"] = function() write(object, arg[4]) end,
-	["read"] = function() read(object) end,
-	["lsver"] = function() ls_versions(object) end,
-	["remove"] = function() remove(object) end,
+  ["readver"] = function() read_version(object, arg[4]) end,
+  ["lsver"] = function() ls_versions(object) end,
+  ["rm"] = function() remove(object) end,
+  ["put"] = function() put(object, arg[4]) end,
+  ["get"] = function() get(object) end,
+  ["rollback"] = function() get(object) end,
 }
 
 actions[command]()
